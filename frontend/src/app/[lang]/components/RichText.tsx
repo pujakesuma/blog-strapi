05655cfbece9bc0eb96d84a0956b064513a72775
Ahmad Ridwan <ahmad.ridwan@kuncie.com>
2023-09-26 11:37:30 +0700

    first commit

---
+++ frontend/src/app/[lang]/components/RichText.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-children-prop */
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+interface RichTextProps {
+  data: {
+    body: string;
+  };
+}
+
+export default function RichText({ data }: RichTextProps) {
+  return (
+    <section className="rich-text py-6 dark:bg-black dark:text-gray-50 ">
+      <Markdown children={data.body} remarkPlugins={[remarkGfm]} />
+    </section>
+  );
+}
